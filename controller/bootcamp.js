b73bb26c1893711f181a8184f662dd4522ece82c
Muhammad Usama Jamil <usamajamil@pk.folio3.com>
2025-04-22 18:03:27 +0500

    create data POST

--- controller/bootcamp.js
+++ controller/bootcamp.js
@@ -1,24 +1,33 @@
-import model from '../model/bootcamp.js'
+import model from "../model/bootcamp.js";
 
-export const getBootcamps = (req,res,next)=>{
-    console.log('req.helo from getBootcamps',req.hello )
-    res.status(200).json({success:true,msg:"Show all bootcamps"})   
-}
+export const getBootcamps = (req, res, next) => {
+  console.log("req.helo from getBootcamps", req.hello);
+  res.status(200).json({ success: true, msg: "Show all bootcamps" });
+};
 
-export const getBootcamp = (req,res,next)=>{
-    res.status(200).json({success:true,msg:`Show bootcamp ${req.params.id}`})
-}
+export const getBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+};
 
-export const createBootcamp = async (req,res,next)=>{
-    const bootcamp = await model.create(req.body)
-    res.status(201).json({success:true,data:bootcamp})
-}
+export const createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await model.create(req.body);
+    res.status(201).json({ success: true, data: bootcamp });
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
+};
 
-export const updateBootcamp = (req,res,next)=>{
-    res.status(200).json({success:true,msg:`Update bootcamp ${req.params.id}`})
-}
+export const updateBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+};
 
-export const deleteBootcamp = (req,res,next)=>{
-    res.status(200).json({success:true,msg:`Delete bootcamp ${req.params.id}`})
-}
-
+export const deleteBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+};
