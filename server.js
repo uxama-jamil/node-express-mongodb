67bd4697fbdc288317ceac7ce301f697e3a465f7
Muhammad Usama Jamil <usamajamil@pk.folio3.com>
2025-04-25 16:49:25 +0500

    formatting

--- server.js
+++ server.js
@@ -1,29 +1,34 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import route from './routes/bootcamps.js'
-import {logger} from './middleware/logger.js'
-import morgan from 'morgan'
-import {connectDB} from './config/db.js'
+import express from "express";
+import dotenv from "dotenv";
+import route from "./routes/bootcamps.js";
+import { logger } from "./middleware/logger.js";
+import morgan from "morgan";
+import { connectDB } from "./config/db.js";
+import { errorHandler } from "./middleware/error.js";
 
-dotenv.config({path:'./config/config.env'})
+dotenv.config({ path: "./config/config.env" });
 
-const app = express()
-connectDB()
+const app = express();
+connectDB();
 
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 //body parser
-app.use(express.json())
-app.use(logger)
-app.use('/api/v1/bootcamps',route)
-const port = process.env.PORT || 5000
-const server = app.listen(port , ()=> console.log(`Server is running in ${process.env.NODE_ENV} on port ${port}`))
+app.use(express.json());
+app.use(logger);
+
+app.use("/api/v1/bootcamps", route);
+app.use(errorHandler);
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () =>
+  console.log(`Server is running in ${process.env.NODE_ENV} on port ${port}`)
+);
 
 // handle unhandled rejection event handling
-process.on('unhandledRejection',(error,promise)=>{
-    // error logs
-    console.log(`Error: ${error.message}`)
-    // close and exit the server
-    server.close(()=>process.exit(1))
-})
+process.on("unhandledRejection", (error, promise) => {
+  // error logs
+  console.log(`Error: ${error.message}`);
+  // close and exit the server
+  server.close(() => process.exit(1));
+});
